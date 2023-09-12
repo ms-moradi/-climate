81ec650f08ea584b2d18fdea5ca3e3381d0f1d89
ms.moradi <ms.moradi28@gmail.com>
2023-09-12 15:33:28 +0330

    use useGetData function

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,18 @@
-import Image from 'next/image'
+'use client'
+
+import { useGetData } from "@/useRequest/useRequest";
+import CityCard from "@/components/cityCard/cityCard";
 
 export default function Home() {
+  const result = useGetData()
+
+  if(result.error) return( <h1> Somthing went wrong! </h1> )
+  if(result.isLoading) return( <h1> Loading... </h1> )
+
+  console.log(result.data.result.main.temp)
   return (
    <>
-   <h1>
-    Hello 
-   </h1>
+    <CityCard result={result}/>
    </>
   )
 }
