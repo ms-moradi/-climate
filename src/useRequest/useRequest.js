e1431c310886847b833552c0df174ca5e397815b
ms.moradi <ms.moradi28@gmail.com>
2023-09-13 21:57:27 +0330

    add function getForecastSixteenDay

--- src/useRequest/useRequest.js
+++ src/useRequest/useRequest.js
@@ -3,12 +3,21 @@
 import useSWR from "swr";
 
 const fetcher = url => fetch(url).then(res => res.json());
-const url = "//one-api.ir/weather/?token=726804:650012cfe76e1&action=current&city=شیراز"
+const url1 = "//one-api.ir/weather/?token=726804:650012cfe76e1&action=current&city=شیراز"
 
 export const useGetData = () =>{
     
-    const { data, error, isLoading } = useSWR(url, fetcher)
+    const { data, error, isLoading } = useSWR(url1, fetcher)
 
     return{ data, error, isLoading}
+}
+
+const url2 = "//one-api.ir/weather/?token=726804:650012cfe76e1&action=daily&city=شیراز"
+
+export const GetForecastSixteenDay = () =>{
+
+    const {data, error, isLoading} = useSWR(url2, fetcher)
+
+    return{data, error, isLoading}
 }
 
