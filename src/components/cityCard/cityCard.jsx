d7c013bc82e791ec440f7f515a9a7bbb06d548f5
ms.moradi <ms.moradi28@gmail.com>
2023-09-13 21:58:43 +0330

    add tags and style

--- src/components/cityCard/cityCard.jsx
+++ src/components/cityCard/cityCard.jsx
@@ -1,10 +1,37 @@
+import picture1 from "@/assets/image/picture-6.jpg";
+import Image from "next/image";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWind, faDroplet, faTemperatureThreeQuarters, faStopwatch, faCircleNotch } from '@fortawesome/free-solid-svg-icons';
 
 
 const CityCard =({result})=>{
+    const temp = (result.data.result.main.temp).toFixed(0)
     return(
         <div>
-            <h1>شیراز</h1>
-            <h2>{result.data.result.main.temp}</h2>
+            <div className="w-full h-[564px] relative">
+                <Image src={picture1} alt="background" className="w-full h-[564px] object-cover mix-blend-darken"/>
+            </div>
+            <div className="w-full h-[564px] text-white bg-zinc-800 flex flex-col justify-between opacity-50 absolute top-0 py-8 px-4">
+                <div className="flex flex-col-reverse">
+                    <div>
+                        <h1 className="text-4xl">shiraz</h1>
+                        <h5 className="text-base">{result.data.result.weather[0]?.main}</h5>
+                    </div>
+                    <div className="flex justify-start gap-1">
+                        <h2 className="text-8xl">{temp}</h2>
+                        <FontAwesomeIcon icon={faCircleNotch} className="text-[12px]"/>
+                    </div>
+                </div>
+                <div>
+                    <div className="w-full h-[1px] bg-white"></div>
+                    <FontAwesomeIcon icon={faWind}  className=""/>
+                    <FontAwesomeIcon icon={faDroplet}  className=""/>
+                    <FontAwesomeIcon icon={faTemperatureThreeQuarters} />
+                    <FontAwesomeIcon icon={faStopwatch} />
+                </div>
+                
+                
+            </div>
         </div>
     )
 }
