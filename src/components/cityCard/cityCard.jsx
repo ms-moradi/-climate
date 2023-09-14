b5eaa04adb4d2333dd27bb1c0ab3dfb52a40660e
ms.moradi <ms.moradi28@gmail.com>
2023-09-14 15:46:30 +0330

    add styles

--- src/components/cityCard/cityCard.jsx
+++ src/components/cityCard/cityCard.jsx
@@ -1,29 +1,30 @@
-import picture1 from "@/assets/image/picture-6.jpg";
+
 import Image from "next/image";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWind, faDroplet, faTemperatureThreeQuarters, faStopwatch, faCircleNotch } from '@fortawesome/free-solid-svg-icons';
-
+import MiniCard from "../miniCard/miniCard";
 
 const CityCard =({result})=>{
     const temp = (result.data.result.main.temp).toFixed(0)
     return(
         <div>
             <div className="w-full h-[564px] relative">
-                <Image src={picture1} alt="background" className="w-full h-[564px] object-cover mix-blend-darken"/>
+                {/* <Image src={picture1} alt="background" className="w-full h-[564px] object-cover mix-blend-darken"/> */}
             </div>
             <div className="w-full h-[564px] text-white bg-zinc-800 flex flex-col justify-between opacity-50 absolute top-0 py-8 px-4">
-                <div className="flex flex-col-reverse">
-                    <div>
-                        <h1 className="text-4xl">shiraz</h1>
-                        <h5 className="text-base">{result.data.result.weather[0]?.main}</h5>
+                <div className="flex flex-row-reverse justify-between items-center">
+                    <div className="flex flex-col items-start justify-baseline">
+                        <h1 className="text-4xl">شیراز</h1>
+                        <h5 className="text-base">{result.data.result.weather[0]?.description}</h5>
                     </div>
-                    <div className="flex justify-start gap-1">
+                    <div className="flex justify-start items-start gap-0">
                         <h2 className="text-8xl">{temp}</h2>
                         <FontAwesomeIcon icon={faCircleNotch} className="text-[12px]"/>
                     </div>
                 </div>
-                <div>
-                    <div className="w-full h-[1px] bg-white"></div>
+                <div className="">
+                    {/* <div className="w-full h-[1px] bg-white"></div> */}
+                    
                     <FontAwesomeIcon icon={faWind}  className=""/>
                     <FontAwesomeIcon icon={faDroplet}  className=""/>
                     <FontAwesomeIcon icon={faTemperatureThreeQuarters} />
