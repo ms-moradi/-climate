4fe571336b11f63e13b8aec0b9e9a019b3437187
ms.moradi <ms.moradi28@gmail.com>
2023-09-14 15:45:57 +0330

    add function and fetch data

--- src/app/weather/page.jsx
+++ src/app/weather/page.jsx
@@ -1,8 +1,21 @@
+'use client'
+
+import { useGetData, GetForecastSixteenDay } from "@/useRequest/useRequest";
+import CityCard from "@/components/cityCard/cityCard";
+import Image from "next/image"; 
+import Loading from "../loading";
 
 const Weather =()=>{
+    const result = useGetData()
+    // const Day = GetForecastSixteenDay()
+  
+    if(result.error) return( <h1> Somthing went wrong! </h1> )
+    if(result.isLoading) return( <Loading/> )
+  
+    console.log(result.data)
     return(
         <div className="w-full bg-[#030303] text-white">
-            Weather
+            weather
         </div>
     )
 }
